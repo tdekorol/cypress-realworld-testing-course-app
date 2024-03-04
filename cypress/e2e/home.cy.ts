81dfb94a394d9f8fc19ee741060214eca6df7eba
Mariia Korol <70329371+tdekorol@users.noreply.github.com>
2024-03-04 14:10:07 -0700

    lesson2

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,12 @@
 describe('home page', () => {
-  it('the h1 contains the correct text', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
-    cy.get("h1")
+  })
+  it('the h1 contains the correct text', () => {
+    cy.get("[data-test='hero-heading']").should("exist").contains("Testing Next.js Applications with Cypress")
+  })
+
+  it('the features on the homepage are correct', () => {
+    cy.get('dt').eq(0).contains('4 Courses')
   })
 })
